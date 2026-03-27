a9d65eb04628a403af315743b53a0eea42d348fd
Admin <vb@viveks-mbp.mynetworksettings.com>
2026-03-27 10:55:18 -0400

    Update lts-app-qr.png and App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { ThemeProvider } from 'next-themes';
-import { AskInstall } from './components/AskInstall';
 
 export default function App() {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <RouterProvider router={router} />
-      <AskInstall />
     </ThemeProvider>
   );
 }
